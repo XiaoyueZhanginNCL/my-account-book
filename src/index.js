2a29af2155d26c6f9cee1caf1f0fe4683dabea9c
XiaoyueZhanginNCL <zhangxiaoyue2022@gmail.com>
2024-02-15 13:45:46 +0000

    redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './theme.css'
+import { Provider } from 'react-redux';
+import store from '@/store/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
     <RouterProvider router={router}></RouterProvider>
+    </Provider>
 );
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,12 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { Button } from "antd-mobile";
+import { useEffect } from "react";
+import { getBillList } from "@/store/modules/billStore";
+import { useDispatch } from "react-redux";
 
 const Layout=()=>{
-    return (<div>
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        dispatch(getBillList())
+    },[dispatch])
+    return (
+    <div>
         <Outlet />
         <div>我是layout</div>
         <Button color="primary">按钮</Button>
-        </div>
+    </div>
     )
 }
 
