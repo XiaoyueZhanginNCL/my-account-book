a1c76eade751dfb668ca3ea64f09b27bb5557cfd
XiaoyueZhanginNCL <zhangxiaoyue2022@gmail.com>
2024-02-15 12:51:04 +0000

    antd-mobile-css

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { Button } from "antd-mobile";
 
 const Layout=()=>{
     return (<div>
         <Outlet />
         <div>我是layout</div>
+        <Button color="primary">按钮</Button>
         </div>
     )
 }
